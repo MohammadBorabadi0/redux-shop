2f3ba92dc377aa1a03522dd7ddc91c96993d6d9b
MohammadBorabadi0 <MohammadBorabadi0@gmail.com>
2022-06-15 18:25:17 -0700

    init commit

---
+++ src/actions.js
@@ -0,0 +1,10 @@
+export const GET_PRODUCTS_REQUEST = "GET_PRODUCTS_REQUEST";
+export const GET_PRODUCTS_SUCCESS = "GET_PRODUCTS_SUCCESS";
+export const GET_PRODUCTS_ERROR = "GET_PRODUCTS_ERROR";
+
+export const ADD_TO_CART = "ADD_TO_CART";
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+export const INCREASE = "INCREASE";
+export const DECREASE = "DECREASE";
+export const CLEAR = "CLEAR";
+export const CHECKOUT = "CHECKOUT";

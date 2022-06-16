05afd54780ef6f255c6e460d375a0d5cde07815a
MohammadBorabadi0 <MohammadBorabadi0@gmail.com>
2022-06-16 05:40:53 -0700

    responsive cartPage

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 
 // Icons
